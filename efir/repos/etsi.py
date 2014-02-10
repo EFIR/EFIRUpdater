686778fc91f0d16d7bdd05864ecc66a83091f1ff
lorenpelli <lorenpelli@hotmail.com>
2014-02-10 09:41:38 +0100

    Definition of the repository

---
+++ efir/repos/etsi.py
@@ -0,0 +1,33 @@
+# ETSI : European Telecommunications Standards Institute
+#
+# Copyright 2014 PwC EU Services
+#
+# Licensed under the EUPL, Version 1.1 or - as soon they
+# will be approved by the European Commission - subsequent
+# versions of the EUPL (the "Licence");
+# You may not use this work except in compliance with the
+# Licence.
+# You may obtain a copy of the Licence at:
+# http://ec.europa.eu/idabc/eupl
+#
+# Unless required by applicable law or agreed to in
+# writing, software distributed under the Licence is
+# distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied.
+# See the Licence for the specific language governing
+# permissions and limitations under the Licence.
+
+from .. import *
+
+NAME = 'etsi'
+URL = "http://webapp.etsi.org/WorkProgram/"
+
+TITLE = 'ETSI : European Telecommunications Standards Institute'
+DESCRIPTION = """ETSI, the European Telecommunications Standards Institute, produces globally-applicable standards for Information and Communications Technologies (ICT), including fixed, mobile, radio, converged, broadcast and internet technologies.
+We are officially recognized by the European Union as a European Standards Organization. The high quality of our work and our open approach to standardization has helped us evolve into a European roots - global branches operation with a solid reputation for technical excellence.
+ETSI is a not-for-profit organization with more than 700 ETSI member organizations drawn from 62 countries across 5 continents world-wide.
+In this section you will find a more detailed description of ETSI, information on how we work, how we are structured, our role and useful information for visiting ETSI.
+Search ETSI standards: http://webapp.etsi.org/WorkProgram/"""
+
+
