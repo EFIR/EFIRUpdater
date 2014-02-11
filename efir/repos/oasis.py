c7b67824cfa19598e78b8309a6a77bfcf5d217f9
Vianney le Clément de Saint-Marcq <vianney.le.clement@be.pwc.com>
2014-02-11 10:28:09 +0100

    oasis: New repository

---
+++ efir/repos/oasis.py
@@ -0,0 +1,103 @@
+# OASIS
+#
+# Copyright 2014 PwC EU Services
+#
+# Licensed under the EUPL, Version 1.1 or - as soon they
+# will be approved by the European Commission - subsequent
+# versions of the EUPL (the "Licence");
+# You may not use this work except in compliance with the
+# Licence.
+# You may obtain a copy of the Licence at:
+# http://ec.europa.eu/idabc/eupl
+#
+# Unless required by applicable law or agreed to in
+# writing, software distributed under the Licence is
+# distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied.
+# See the Licence for the specific language governing
+# permissions and limitations under the Licence.
+
+from .. import *
+import re
+import mimetypes
+
+NAME = 'oasis'
+URL = "https://www.oasis-open.org/standards"
+
+TITLE = 'Organization for the Advancement of Structured Information Standards - OASIS'
+DESCRIPTION = """OASIS (Organization for the Advancement of Structured Information Standards) is a non-profit consortium that drives the development, convergence and adoption of open standards for the global information society.
+
+OASIS promotes industry consensus and produces worldwide standards for security, Cloud computing, SOA, Web services, the Smart Grid, electronic publishing, emergency management, and other areas. OASIS open standards offer the potential to lower cost, stimulate innovation, grow global markets, and protect the right of free choice of technology. """
+
+PUBLISHER = Publisher(URIRef("https://www.oasis-open.org/org"))
+PUBLISHER.name = Literal("OASIS", lang="en")
+PUBLISHER.type = PublisherType.NonProfitOrganisation
+
+LICENSE = LicenseDocument(URIRef("https://www.oasis-open.org/policies-guidelines/ipr"))
+LICENSE.label = Literal("OASIS Intellectual Property Rights (IPR) Policy", lang="en")
+LICENSE.type = LicenceType.NoDerivativeWork
+
+
+def get_date(tag):
+    '''Return the date contained in one of the <p> children of tag.'''
+    for p in tag.find_all('p'):
+        try:
+            return datetime.datetime.strptime(p.text.strip(), '%B %Y')
+        except ValueError:
+            pass
+    return None
+
+
+def get_asset_rows(page):
+    '''Return the list of rows corresponding to assets.'''
+    table = page.find(class_="node-inner").table
+    return table.find_all('tr')[1:]
+
+
+def get_asset(page, tr):
+    '''Generate the asset from row tr.'''
+    standard, producer, approved = tr.find_all('td')
+    for a in standard.find_all('a', attrs={'name':True}):
+        name = a.attrs["name"]
+        link = page.find('a', href='#'+name)
+        if link:
+            title = link.text
+            break
+    logging.debug("Parsing asset %s.", name)
+    asset = Asset(URIRef(URL + '#' + name))
+    asset.title = Literal(title, lang="en")
+    asset.description = Literal(standard.p.text, lang="en")
+    asset.status = Status.Completed
+    asset.modified = get_date(approved)
+    asset.versionInfo = re.search(r"([0-9.]*)(?: \(.*\)|: .*)?$", title).group(1)
+    asset.language = Language.en
+    asset.publisher = PUBLISHER
+    asset.theme = Eurovoc.term("100223")
+    asset.type = AssetType.Schema
+    asset.distribution = set()
+    for a in standard.find_all('a'):
+        if 'href' not in a.attrs:
+            continue
+        d = AssetDistribution(URIRef(urljoin(URL, a['href'])))
+        d.accessURL = d.uri
+        d.status = asset.status
+        d.description = Literal(a.text, lang="en")
+        d.license = LICENSE
+        mime = mimetypes.guess_type(str(d.accessURL))[0]
+        if mime:
+            d.mediaType = MediaType.term(mime)
+        asset.distribution.add(d)
+    return asset
+
+
+def process():
+    page = HTMLPage(NAME, URL)
+    repo = Repository(URIRef(URL))
+    repo.accessURL = URIRef(URL)
+    repo.title = Literal(TITLE, lang="en")
+    repo.description = Literal(DESCRIPTION, lang="en")
+    repo.publisher = PUBLISHER
+    repo.modified = get_modified(NAME, URL)
+    repo.dataset = {get_asset(page, row) for row in get_asset_rows(page)}
+    return repo
